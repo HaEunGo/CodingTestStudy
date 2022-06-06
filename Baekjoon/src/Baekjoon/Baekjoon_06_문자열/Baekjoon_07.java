9e125eb0dec756b381f2f0dbaf7fe48f05ccdeb6
haeun <gohe0301@gmail.com>
2022-06-06 18:23:21 +0900

    2022 06 06 - Baekjoon(BOJ) : 2908_상수

--- Baekjoon/src/Baekjoon/Baekjoon_06_문자열/Baekjoon_07.java
+++
@@ -1,9 +0,0 @@
-package Baekjoon.Baekjoon_06_문자열;
-
-public class Baekjoon_02 {
-
-	public static void main(String[] args) {
-		// https://www.acmicpc.net/problem/11720
-		// 3. 10809_알파벳 찾기
-	}
-}
---
+++ Baekjoon/src/Baekjoon/Baekjoon_06_문자열/Baekjoon_07_2908_상수.java
@@ -0,0 +1,33 @@
+package Baekjoon.Baekjoon_06_문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_07_2908_상수 {
+
+	public static void main(String[] args) throws IOException {
+		// https://www.acmicpc.net/problem/2908
+		// 7. 2908_상수
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// 상수 입력
+		// -> 거꾸로 입력(StringBuffer) -> 다시 int로
+		// -> 거꾸로 입력한 값으로 대소 비교
+		
+		String[] arr = br.readLine().split(" ");
+		
+		StringBuffer num1 = new StringBuffer(arr[0]).reverse();
+		StringBuffer num2 = new StringBuffer(arr[1]).reverse();
+		
+		int intNum1 = Integer.parseInt(num1.toString());
+		int intNum2 = Integer.parseInt(num2.toString());
+
+		if(intNum1 > intNum2) {
+			System.out.println(intNum1);
+		} else {
+			System.out.println(intNum2);
+		}
+	}
+}
