474728b73b4f5a724e7b3c40af3095bb6da66259
haeun <gohe0301@gmail.com>
2022-06-19 20:43:44 +0900

    2022 06 19 - Baekjoon(BOJ) : 2292_벌집(ing)

---
+++ Baekjoon/src/Baekjoon/Baekjoon_07_기본_수학_1/Baekjoon_02_2292_벌집.java
@@ -0,0 +1,18 @@
+package Baekjoon.Baekjoon_07_기본_수학_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_02_2292_벌집 {
+
+	public static void main(String[] args) throws IOException {
+		// https://www.acmicpc.net/problem/2292
+		// 2. 2292_벌집
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+	}
+}
