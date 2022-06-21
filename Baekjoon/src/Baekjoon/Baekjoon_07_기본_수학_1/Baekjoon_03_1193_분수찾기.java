411ce1451d0ddeecd7534ba86b439da4e2551650
haeun <gohe0301@gmail.com>
2022-06-21 17:44:24 +0900

    2022 06 21 - Baekjoon(BOJ) : 1193_분수찾기

---
+++ Baekjoon/src/Baekjoon/Baekjoon_07_기본_수학_1/Baekjoon_03_1193_분수찾기.java
@@ -0,0 +1,34 @@
+package Baekjoon.Baekjoon_07_기본_수학_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_03_1193_분수찾기 {
+
+	public static void main(String[] args) throws IOException {
+		// https://www.acmicpc.net/problem/1193
+		// 3. 1193_벌집
+		
+		// https://thinmug.tistory.com/9 참고
+		// https://st-lab.tistory.com/74
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		int n = 0;
+		while(true) {
+			n++;
+			cnt += n;
+			
+			if(cnt >= a) {
+				if(n % 2 == 0) {
+					System.out.println(a - cnt + n + "/" + (cnt - a + 1));
+				} else {
+					System.out.println((cnt - a + 1) + "/" + (a - cnt + n));
+				}
+				break;
+			}
+		}
+	}
+}
