d0648230e38479351ccf459c932485d48b3a1a38
haeun <gohe0301@gmail.com>
2022-06-24 15:15:23 +0900

    22 06 24 - Baeckjoon(BOJ) : 10250_ACM_호텔(ing)

---
+++ Baekjoon/src/Baekjoon/Baekjoon_07_기본_수학_1/Baekjoon_05_10250_ACM_호텔.java
@@ -0,0 +1,18 @@
+package Baekjoon.Baekjoon_07_기본_수학_1;
+
+public class Baekjoon_05_10250_ACM_호텔 {
+
+	public static void main(String[] args) {
+		// https://www.acmicpc.net/problem/10250
+		// 5. 10250_ACM 호텔
+		
+		// 테스트 케이스 T
+		// 층 수 H / W개의 방 / 몇 번째 손님 N
+		// 방 번호는 YXX, YYXX 형태 / Y, YY = 층 수 / XX = 엘레베이터에서 부터 세었을 때
+		// 그럼 X먼저 H를 채운다. (101 ~ Y01 / YY01)
+		//
+		
+		
+		
+	}
+}
