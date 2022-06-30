946966014e4e3b15a383c5bfde0fde31b1de9f91
haeun <gohe0301@gmail.com>
2022-06-30 21:34:24 +0900

    2022 06 30 - Baeckjoon(BOJ) : 2839_설탕_배달(ing)

---
+++ Baekjoon/src/Baekjoon/Baekjoon_07_기본_수학_1/Baekjoon_07_2839_설탕_배달.java
@@ -0,0 +1,14 @@
+package Baekjoon.Baekjoon_07_기본_수학_1;
+
+public class Baekjoon_07_2839_설탕_배달 {
+
+	public static void main(String[] args) {
+		// https://www.acmicpc.net/problem/2839
+		// 7. 2839_설탕 배달
+		
+		// 설탕 Nkg -> 3, 5의 배수
+		// 3 <= N <= 5000
+		// N % 3 || N % 5 != 0 -> -1 출력
+		
+	}
+}
